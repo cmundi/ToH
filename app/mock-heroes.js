eac1ab1e154508fd2b96d2d03acf47cda8c8a40c
C. Mundi <cmundi@gmail.com>
2017-01-23 09:45:51 -0700

    working through section 6 (Routing)
    WIP; about to start "Select a Dashboard Hero" section of https://angular.io/docs/ts/latest/tutorial/toh-pt5.html

---
+++ app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Superman' },
+    { id: 12, name: 'Batman' },
+    { id: 13, name: 'Green Lantern' },
+    { id: 14, name: 'Spider Man' },
+    { id: 15, name: 'Wonder Woman' },
+    { id: 16, name: 'Aqua Man' },
+    { id: 17, name: 'The Flash' },
+    { id: 18, name: 'Bat Girl' },
+    { id: 19, name: 'Freeze' },
+    { id: 20, name: 'Elastic Girl' }
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman' },
+  { id: 12, name: 'Batman' },
+  { id: 13, name: 'Green Lantern' },
+  { id: 14, name: 'Spider Man' },
+  { id: 15, name: 'Wonder Woman' },
+  { id: 16, name: 'Aqua Man' },
+  { id: 17, name: 'The Flash' },
+  { id: 18, name: 'Bat Girl' },
+  { id: 19, name: 'Freeze' },
+  { id: 20, name: 'Elastic Girl' }
+];
